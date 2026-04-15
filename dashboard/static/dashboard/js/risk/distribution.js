2c91fe2194263e49767a05c737a6d5451c639d31
gombakudzaicr-eng <gombakudzaicr@gmail.com>
2026-04-15 15:21:19 +0200

    dashboard table headings

--- dashboard/static/dashboard/js/risk/distribution.js
+++ dashboard/static/dashboard/js/risk/distribution.js
@@ -49,7 +49,7 @@ const buildDistributionOption = (rows, width) => ({
             color: "#52677c",
             fontSize: width < 720 ? 10 : 11,
             interval: 0,
-            rotate: width < 720 ? 16 : 0,
+            rotate:  0,
         },
     },
     yAxis: {
