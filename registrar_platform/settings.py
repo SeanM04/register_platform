5e684deb5cc277acbd32f4ffaba70993d19021b5
maccay <makanakakanyai@gmail.com>
2026-04-16 15:00:18 -0700

    Configure SQLite database for local development

--- registrar_platform/settings.py
+++ registrar_platform/settings.py
@@ -97,12 +97,8 @@ WSGI_APPLICATION = 'registrar_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'registrar_dashboard'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('POSTGRES_HOST', '127.0.0.1'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
